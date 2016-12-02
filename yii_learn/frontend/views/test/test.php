06ed39f6f1fadbe9f31417da6354ba207ac7b7ba
Yoke123 <2514407937@qq.com>
2016-12-02 14:48:07 +0800

    add /yii2-jui

--- yii_learn/frontend/views/test/test.php
+++ yii_learn/frontend/views/test/test.php
@@ -1,3 +1,12 @@
 <?php 
 
-echo "test";
+use yii\jui\DatePicker;
+echo "test <br />";
+
+echo DatePicker::widget([
+		'language' => 'zh-CN',
+		'name' => 'country',
+		'clientOptions' => [
+			'dataFormat' => 'yy-mm-dd',
+		],
+]);
