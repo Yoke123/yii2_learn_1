aa27722822854b397ac491facf4bc6026c17c80a
Yoke123 <2514407937@qq.com>
2016-12-15 16:23:45 +0800

    update code

--- yii_learn/frontend/views/test/test.php
+++ yii_learn/frontend/views/test/test.php
@@ -19,3 +19,19 @@ echo Html::button('button', ['class' => 'btn btn-success']);
 echo "<br/>";
 
 echo Test::widget();
+echo "<br/>";
+
+// $str = file_get_contents('http://www.manks.top');
+
+
+
+function getHtmlContents($url)
+{
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_exec($ch);
+	curl_close($ch);
+}
+
+getHtmlContents('www.manks.top');
